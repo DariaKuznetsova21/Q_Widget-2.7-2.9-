b17cbdd8023cf96ac8b5f07b84f4cb89364ab834
Daria <dariakuznetsova21@gmail.com>
2024-03-26 23:25:40 +0700

    Remove Lab 2.9

---
+++ MoveFigures/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+
+#include "window.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication appl(argc, argv);
+    Window win;
+    win.show();
+    return appl.exec();
+}
